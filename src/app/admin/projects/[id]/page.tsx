2c488cd51d42b1ecdebc75b5420700341180db61
Muhammed Furkan Ardıç <furkanardcm@gmail.com>
2025-01-01 23:20:05 +0300

    fix: generateMetadata'da params kullanıldı

--- src/app/admin/projects/[id]/page.tsx
+++ src/app/admin/projects/[id]/page.tsx
@@ -1,11 +1,13 @@
 import EditProjectForm from '@/app/admin/projects/[id]/EditProjectForm';
+import { Metadata } from 'next';
 
-interface Props {
-  params: {
-    id: string;
+export async function generateMetadata({ params }: { params: { id: string } }): Promise<Metadata> {
+  return {
+    title: `Proje #${params.id} Düzenle | Admin Panel`,
+    description: `${params.id} ID'li projeyi düzenleme sayfası`
   };
 }
 
-export default function EditProjectPage({ params }: Props) {
+export default function EditProjectPage({ params }: { params: { id: string } }) {
   return <EditProjectForm id={params.id} />;
 } 
